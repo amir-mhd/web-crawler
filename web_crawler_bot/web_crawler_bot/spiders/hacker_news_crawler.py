9ac0466b78ca200e64f4b832321e5ae20ab16a10
amir-mhd <amirmohammadian@hotmail.com>
2020-10-26 23:32:27 +0330

    writing an spider which get the title and publisher of hackernews posts

--- web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
+++ web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
@@ -3,13 +3,20 @@ import scrapy
 class Crawler(scrapy.Spider):
     name = "hackernews"
 
-    start_urls = ["https://news.ycombinator.com/"]
+    start_urls = ["https://news.ycombinator.com/",
+                  "https://news.ycombinator.com/news?p=2",
+                   "https://news.ycombinator.com/news?p=3"
+    ]
 
 
     def parse(self, response):
         titles = response.css("td.title a")
         for title in titles:
             yield{
-                "title": title.css("a::text").getall()
+                "title": title.css("a::text").get(),
+                "publisher": title.css("span.sitestr::text").get()
             }         
 
+        
+                    
+            
