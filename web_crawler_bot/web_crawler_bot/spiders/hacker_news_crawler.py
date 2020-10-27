4a4128843c9defffd3a124331f3ae4d3fdc8e0f1
amir-mhd <amirmohammadian@hotmail.com>
2020-10-27 22:50:27 +0330

    fixing some portion of csv bug

--- web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
+++ web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
@@ -9,25 +9,24 @@ class Hackernews_crawler(scrapy.Spider):
     # determine which page should be the starting page
     start_urls = ["https://news.ycombinator.com/"]
 
-    # settings for string downloaded data
+    # settings for storing downloaded data
     custom_settings = {
         "FEED_FORMAT": "csv", "FEED_URI": "hackernews_data.csv"}
 
     # defining a method to parse the html and css in pages
     def parse(self, response):
         article_item = Hackernews_items() 
-        articles_info = response.css("td.title a")
+        articles_info = response.css("td.title")
         for article in articles_info:
             article_item["title"] = article.css("a::text").get()
             article_item["publisher"] = article.css("span.sitestr::text").get()
-    
             yield article_item
+    
         
         # get the next page link
         next_page = response.css("a.morelink::attr(href)").get()
-        # check if there was another page crawl that too
+        # check if there was an another page crawl that too
         if next_page is not None:
             for a in response.css("a.morelink"):
-                # follow just is a shortcut to go to next relative URLs
+                # follow is just a shortcut of joinurl and it goes to the next relative URLs
                 yield response.follow(a, callback=self.parse)
-
