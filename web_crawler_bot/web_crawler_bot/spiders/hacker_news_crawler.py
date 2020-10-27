4e682bc15f22bbdda699ebe124f69bab0871aaa5
amir-mhd <amirmohammadian@hotmail.com>
2020-10-27 11:00:17 +0330

    adding some useful comments

--- web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
+++ web_crawler_bot/web_crawler_bot/spiders/hacker_news_crawler.py
@@ -1,14 +1,13 @@
 import scrapy
 
 class Crawler(scrapy.Spider):
+    # assign a name to later call it in crawl command
     name = "hackernews"
 
-    start_urls = ["https://news.ycombinator.com/",
-                  "https://news.ycombinator.com/news?p=2",
-                   "https://news.ycombinator.com/news?p=3"
-    ]
+    # determine which page should be the starting page
+    start_urls = ["https://news.ycombinator.com/"]
 
-
+    # defining a method to parse the html and css in pages
     def parse(self, response):
         titles = response.css("td.title a")
         for title in titles:
@@ -17,9 +16,11 @@ class Crawler(scrapy.Spider):
                 "publisher": title.css("span.sitestr::text").get()
             }         
 
+        # get the next page link
         next_page = response.css("a.morelink::attr(href)").get()
-        
+        # check if there was another page crawl that too
         if next_page is not None:
             for a in response.css("a.morelink::attr(href)"):
+                # follow just is a shortcut to go to next relative URLs
                 yield response.follow(a, callback=self.parse)
             
